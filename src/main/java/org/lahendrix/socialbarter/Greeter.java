51049ed369d5973cc092b9658e1e9d85a335caf9
Larry A. Hendrix <larry.hendrix@dealer.com>
2013-11-07 15:20:11 -0500

    Updated pom and other config info.

---
+++ src/main/java/org/lahendrix/socialbarter/Greeter.java
@@ -0,0 +1,20 @@
+package org.lahendrix.socialbarter;
+
+public class Greeter {
+	
+	private String message;
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String greet() {
+		return message;
+	}
+	
+	
+}
