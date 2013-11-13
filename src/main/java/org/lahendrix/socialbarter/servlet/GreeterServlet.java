1f93879b2e6ba9c7b026f1b8bf39080831661c94
Kevin Thorley <kevin.thorley@dealer.com>
2013-11-13 11:51:00 -0500

    Add basic servlets and data source support

--- src/main/java/org/lahendrix/socialbarter/servlet/GreeterServlet.java
+++ src/main/java/org/lahendrix/socialbarter/servlet/GreeterServlet.java
@@ -2,17 +2,37 @@ package org.lahendrix.socialbarter.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.lahendrix.socialbarter.Greeter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class GreeterServlet extends HttpServlet {
 
+	@Autowired
+	private Greeter greeter;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+//		WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(config.getServletContext());
+//		this.greeter = context.getBean("greeter", Greeter.class);
+		
+		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, config.getServletContext());
+	}
+
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		resp.getWriter().println("Hello World");
+		resp.getWriter().println(this.greeter.greet());
 	}
-
 }
